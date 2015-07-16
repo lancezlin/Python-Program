f849c373ec71974eb638f1331dbe5f45d89b988f
Lance Lin <linzhengzhong@icloud.com>
2015-07-16 10:44:48 -0700

    Update readFile.py
    add question

--- readFile.py
+++ readFile.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Write a program to read through a file and print the contents of the
+file (line by line) all in upper case.
+@author: lancezlin
 """
 
 fname = raw_input('Type a file name to read: \n')
