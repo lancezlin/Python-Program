85393ec8bc97d80b490486f6cdd46058489bedf9
Lance Lin <linzhengzhong@icloud.com>
2015-08-05 17:54:25 -0700

    Create lintcode196.py
    Find the Missing Number

---
+++ lintcode196.py
@@ -0,0 +1,30 @@
+'''
+Given an array contains N numbers of 0 .. N, find which number doesn't exist in the array.
+Example:
+Given N = 3 and the array [0, 1, 3], return 2.
+
+'''
+class Solution:
+    # @param nums: a list of integers
+    # @return: an integer
+    def findMissing(self, nums):
+        # write your code here
+        '''
+        missV = 0
+        while missV < len(nums):
+            nums.append(missV)
+            if nums.count(missV) == 2:
+                missV += 1
+                nums.pop(-1)
+            else:
+                #missV += 1
+                break
+        return missV
+        '''
+        missV = None
+        nums.sort()
+        for i in xrange(0, len(nums)+1, 1):
+            if nums.count(i) != 1:
+                missV = i
+        return missV
+                
