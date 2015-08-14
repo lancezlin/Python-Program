2233b1c811b059f8442c8a9be8ab19ca66167243
Lance Lin <linzhengzhong@icloud.com>
2015-08-13 17:42:55 -0700

    Create LinkedStack.py
    implement stack with a singly linked list.

---
+++ LinkedStack.py
@@ -0,0 +1,35 @@
+class LinkedStack:
+	"""LIFO Stack implementation using a singly linked list for storage"""
+	__slots__ = '_element', '_next'
+	class _Node(object):
+		"""docstring for Node"""
+		def __init__(self, element, next):
+			self.element = element
+			self.next = next
+	
+	def __init__(self):
+		self._head = None
+		self._size = 0
+	
+	def __len__(self):
+		return self._size
+	
+	def is_empty(self):
+		return self._size == 0
+	
+	def push(self, e):
+		self._head = self._Node(e, self._head)
+		self._size += 1
+
+	def top(self):
+		if self.is_empty():
+			raise Empty('Stack is empty')
+		return self._head._element
+
+	def pop(self):
+		if self.is_empty():
+			raise Empty('Stack is empty')
+		answer = self._head._element
+		self._head = self._head._next
+		self._size -= 1
+		return answer
