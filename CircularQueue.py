f27743eb5b8fac02611d72cab5ec675596814ef6
Lance Lin <linzhengzhong@icloud.com>
2015-08-14 16:48:58 -0700

    Create CircularQueue.py
    to be continued.

---
+++ CircularQueue.py
@@ -0,0 +1,28 @@
+class CircularQueue:
+	"""docstring for CircularQueue"""
+	class _Note:
+		__slots__ = '_element', '_next'
+		def __init__(self.element, next):
+			self._element = element
+			self._next = next
+
+	def __init__(self):
+		self._tail = None
+		self._size = 0
+
+	def __len__(self):
+		return self._size
+
+	def is_empty(self):
+		return self._size == 0
+
+	def first(self):
+		if self.is_empty():
+			raise Empty('Queue is empty')
+		head = self._tail._next
+		return head._element
+
+	def dequeue(self):
+		if self.is_empty():
+			raise Empty('Queue is empty')
+		
