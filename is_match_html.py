62035c5fb3f9c3be1b8ce472f3790cd22271e9dd
Lance Lin <linzhengzhong@icloud.com>
2015-08-26 23:27:25 -0700

    Create is_match_html.py
    Using stack to check HTML is matched with right tags..

---
+++ is_match_html.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 22:28:55 2015
+
+@author: Lance
+"""
+
+def is_matched_html(raw):
+    S = ArrayStack()
+    j = raw.find('<')
+    while j != -1:
+        k = raw.find('>', j+1)
+        if k == -1:
+            return False
+        tag = raw[j+1:k]
+        if not tag.startwith('/'):
+            S.push(tag)
+        else:
+            if S.is_empty():
+                return False
+            if tag[1:] != S.pop():
+                return False
+        j = raw.find('<', k+1)
+    return S.is_empty()
