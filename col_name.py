c6ad3e49be02073065ef7b292d0b96eb4999abfd
Lance Lin <linzhengzhong@icloud.com>
2015-09-22 11:04:32 -0700

    fix typo
    "sting" to "string"

--- col_name.py
+++ col_name.py
@@ -2,7 +2,7 @@ import pandas as pd
 def col_cont_str(df, string): # df is data frame, string is a key word or sub key word
     _col = []
     for col in list(df.columns.values):
-        if sting in col:
+        if string in col:
             _col.append(col) # e.g. get all column names contains PTB rate
         else:
             continue
