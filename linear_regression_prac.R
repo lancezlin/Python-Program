07e1cb4783fb40ad5998fc0f1f259334f915f960
lancezlin <linzhengzhong@icloud.com>
2015-12-07 23:33:42 -0800

    plotting LG model

--- linear_regression_prac.R
+++ linear_regression_prac.R
@@ -7,6 +7,19 @@ names(Boston)
 lm <- lm(medv ~ lstat, data=Boston)
 attach(Boston)
 summary(lm)
-names(lm)
-#lm$coefficients[1]
+names(lm)  #lm$coefficients[1]
 res <- lm$residuals
+confint(lm, level = 0.95) # obtain a CI for coefficients
+predict(lm, data.frame(lstat=(c(6, 8, 17))), interval="confidence")
+predict(lm, data.frame(lstat=(c(6, 8, 17))), interval="prediction")
+# plot
+plot(lstat, medv)
+abline(lm, lwd = 3, col="red")
+
+par(mfrow=c(2,2))
+plot(lm)
+plot(predict(lm), residuals(lm))
+plot(predict(lm), rstudent(lm))
+plot(hatvalues(lm))
+which.max(hatvalues(lm))
+
